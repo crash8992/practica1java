26002d3a1656c365b212842106ccb96fde80babe
crash8992 <crash899@gmail.com>
2022-06-04 21:55:37 -0500

    prueba subida 1

---
+++ practica2.java
@@ -0,0 +1,19 @@
+/**
+ * Class Untitled
+ */
+public class Practica1 {
+	public static void main(String args[]){
+        int miVariableEntera =10;
+        System.out.println(miVariableEntera);
+        //Modificamos el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+        //Cambio Variable adios
+        miVariableCadena = "Adios";
+        System.out.println(miVariableCadena);
+
+    }
+}
